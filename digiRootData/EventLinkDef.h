8ecbb4bb74183b9e817948c917dd99f6b8c17e33
heather <>
2001-10-16 13:28:22 +0000

    add + in LinkDef header file...needed to use streamers correctly

--- digiRootData/EventLinkDef.h
+++ digiRootData/EventLinkDef.h
@@ -4,21 +4,21 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class AcdTile;
-#pragma link C++ class CalLog;
-#pragma link C++ class ESAPID;
-#pragma link C++ class DigiEvent;
-#pragma link C++ class L1T;
-#pragma link C++ class LogId;
-#pragma link C++ class StripId;
-#pragma link C++ class Tagger;
-#pragma link C++ class AcdId;
-#pragma link C++ class TkrLayer;
-#pragma link C++ class SubSystemHeader;
-#pragma link C++ class CalHeader;
-#pragma link C++ class AcdHeader;
-#pragma link C++ class TkrHeader;
-#pragma link C++ class LiveTime;
+#pragma link C++ class AcdTile+;
+#pragma link C++ class CalLog+;
+#pragma link C++ class ESAPID+;
+#pragma link C++ class DigiEvent+;
+#pragma link C++ class L1T+;
+#pragma link C++ class LogId+;
+#pragma link C++ class StripId+;
+#pragma link C++ class Tagger+;
+#pragma link C++ class AcdId+;
+#pragma link C++ class TkrLayer+;
+#pragma link C++ class SubSystemHeader+;
+#pragma link C++ class CalHeader+;
+#pragma link C++ class AcdHeader+;
+#pragma link C++ class TkrHeader+;
+#pragma link C++ class LiveTime+;
 
 
 #endif
