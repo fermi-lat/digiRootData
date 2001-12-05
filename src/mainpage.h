ef9f256f7706246349f9e9dbf14e2952c7c7c702
heather <>
2001-12-05 14:46:26 +0000

    moved from header directory

---
+++ src/mainpage.h
@@ -0,0 +1,16 @@
+// Mainpage for doxygen
+
+/*! \mainpage package digiRootData
+
+  \section intro Introduction
+  This package defines the ROOT classes used to store GLAST digitization data
+
+  <hr>
+  \section notes release notes
+  \include release.notes
+  \section requirements requirements
+  \include requirements
+
+  \authors Dan Flath and Heather Kelly
+*/
+
