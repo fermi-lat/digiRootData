ec1f35371a1321ff7ea903469d3d04dd4a35e0fb
cvs2git <>
2002-03-14 15:56:17 +0000

    This commit was manufactured by cvs2svn to create tag 'v1r1p2'.
    Sprout from testbeam 2002-03-14 15:55:52 UTC heather 'automatic rootcint for windows'
    Delete:
        digiRootData/EventCint.cxx
        digiRootData/EventCint.h
        digiRootData/dorootcint.bat

--- digiRootData/dorootcint.bat
+++
@@ -1,3 +0,0 @@
-REM do rootcint for Event classes
-cd ..\digiRootData
-%ROOTSYS%\bin\rootcint -f EventCint.cxx -c ACDTile.h CalHit.h ESAPID.h Event.h L1T.h LogID.h StripID.h Tagger.h TileID.h TkrLayer.h EventLinkDef.h
