b15deab78f8f0c837bbc855585421e94d537a6b3
heather <>
2002-04-05 19:34:58 +0000

    removing old dorootcint.bat file

--- digiRootData/dorootcint.bat
+++
@@ -1,3 +0,0 @@
-REM do rootcint for Event classes
-cd ..\digiRootData
-%ROOTSYS%\bin\rootcint -f EventCint.cxx -c AcdHeader.h AcdId.h AcdTile.h CalHeader.h CalLog.h ESAPID.h DigiEvent.h L1T.h LiveTime.h LogId.h StripId.h SubSystemHeader.h Tagger.h TkrHeader.h TkrLayer.h EventLinkDef.h
