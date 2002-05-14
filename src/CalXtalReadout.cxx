eb967c0b5da869f905d8d668d72fb401b8ef43ea
heather <>
2002-05-14 15:16:02 +0000

    fix bug in CalXtalReadout - setting rangeM incorrectly

--- src/CalXtalReadout.cxx
+++ src/CalXtalReadout.cxx
@@ -16,7 +16,7 @@ CalXtalReadout::CalXtalReadout() {
 
 CalXtalReadout::CalXtalReadout(Char_t rangeP, UShort_t adcP, Char_t rangeM, 
                                UShort_t adcM) :
-m_rangeP(rangeP), m_adcP(adcP), m_rangeM(adcM), m_adcM(adcM)
+m_rangeP(rangeP), m_adcP(adcP), m_rangeM(rangeM), m_adcM(adcM)
 {
 }
 
