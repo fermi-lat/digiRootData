38ce02bf2e7fa3cf6f156ef9ab305f5080b0a997
heather <>
2002-05-14 15:45:28 +0000

    update doc

--- src/mainpage.h
+++ src/mainpage.h
@@ -1,16 +1,23 @@
-// Mainpage for doxygen
+/** @mainpage package digiRootData
 
-/*! \mainpage package digiRootData
-
-  \section intro Introduction
-  This package defines the ROOT classes used to store GLAST digitization data
+  @section intro Introduction
+  This package defines the ROOT classes used to store GLAST digitization data.
+  The main branch is defined as the DigiEvent class.  This class contains:
+  - Run Id
+  - Event Id
+  - Collection of CalDigi objects.
+    -# Mode:  BESTRANGE or ALLRANGE
+    -# CalXtalId - packed UInt_t storing tower, layer, column
+    -# Collection of one (BESTRANGE) or four (ALLRANGE) CalXtalReadout objects\n
+       Each readout contains:  range and ADC values for each end (POS and NEG).
+       
 
   <hr>
-  \section notes release notes
-  \include release.notes
-  \section requirements requirements
-  \include requirements
+  @section notes release notes
+  @include release.notes
+  @section requirements requirements
+  @include requirements
 
-  \authors Dan Flath and Heather Kelly
+  @authors Dan Flath and Heather Kelly
 */
 
