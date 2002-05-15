9365e9bc7ca1ba6853b7860a95b67ad5f29cc588
heather <>
2002-05-15 19:38:32 +0000

    update bat file, removing references to mcRootData

--- cmt/dorootcint.bat
+++ cmt/dorootcint.bat
@@ -1,7 +1,7 @@
-REM do rootcint for MC ROOT classes, only if either Cint file
+REM do rootcint for DIGI ROOT classes, only if either Cint file
 REM is missing
-if not exist %MCROOTDATAROOT%\mcRootData\mcRootDataCint.cxx goto :doit
-if exist %MCROOTDATAROOT%\mcRootData\mcRootDataCint.h goto :exit
+if not exist %DIGIROOTDATAROOT%\digiRootData\digiRootDataCint.cxx goto :doit
+if exist %DIGIROOTDATAROOT%\digiRootData\digiRootDataCint.h goto :exit
 :doit
 REM Save the current directory, so we can return when finished
 cd > dirstor.txt
@@ -9,9 +9,6 @@ copy %DIGIROOTDATAROOT%\cmt\cd.cmd+dirstor.txt %DIGIROOTDATAROOT%\changeDir.bat
 del dirstor.txt
 
 cd %DIGIROOTDATAROOT%\digiRootData
-REM Remove old cint files
-del EventCint.cxx
-del EventCint.h
 
 %ROOT_PATH%\bin\rootcint -f digiRootDataCint.cxx -c CalDigi.h CalXtalId.h CalXtalReadout.h DigiEvent.h EventLinkDef.h
 
