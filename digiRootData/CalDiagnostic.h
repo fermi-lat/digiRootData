09bb375614bf17603e8cc6bc131ddf777b89e313
heather <>
2003-12-18 21:05:32 +0000

    reactivate diagnostic data - using Online's EBF diagnostic interface

--- digiRootData/CalDiagnostic.h
+++ digiRootData/CalDiagnostic.h
@@ -1,56 +1,38 @@
-#ifndef ROOT_CalDiagnostic_H
-#define ROOT_CalDiagnostic_H
+#ifndef ROOT_CalDiagnosticData_H
+#define ROOT_CalDiagnosticData_H
 
 #include "TObject.h"
 
-/** @class CalDiagnostic
- * @brief Store the EM CalDiagnostic trigger primitives
+/** @class CalDiagnosticData
+ * @brief Store the EM CalDiagnosticData trigger primitives
  *
  * $Header$
 */
 
-class CalDiagnostic : public TObject {
+class CalDiagnosticData : public TObject {
 public:
             
-    CalDiagnostic();
-    CalDiagnostic(UInt_t datum, Short_t gccc, Short_t layer);
-    virtual ~CalDiagnostic();
+    CalDiagnosticData();
+    CalDiagnosticData(UInt_t datum);
+    virtual ~CalDiagnosticData();
 
-    void initialize(UInt_t datum, Short_t gccc, Short_t layer);
+    void initialize(UInt_t datum);
 
     void Clear(Option_t *option ="");
 
     void Print(Option_t *option="") const;
 
-    UShort_t getGccc() const { return m_gccc; };
-    UShort_t getLayer() const { return m_layer; };
-
     UInt_t getDataWord() const { return m_datum; };
-
-    UInt_t getCalHigh(UInt_t end=0) const
-     { // end == 0 for positive end, 1 for negative
-            return (m_datum >> (13 + (end<<4))) & 0x001;
-      }
-
-    UInt_t getCalLow(UInt_t end) const
-        {
-            return (m_datum >> (12 + (end<<4))) & 0x001;
-        }
-
-     UInt_t getCalLogAccepts(UInt_t end) const
-        {
-            // end == 0 is positive, 1 is negative
-            return (m_datum >> (end<<4)) & 0xfff;
-        }
+    UInt_t high(UInt_t sign) const;
+    UInt_t low(UInt_t sign) const;
+    UInt_t logAccepts(UInt_t sign) const;
 
 
 private:
     /// packed word containing trigger primitives for this set of logs
     UInt_t m_datum;
-    UShort_t m_gccc;
-    UShort_t m_layer;
 
-    ClassDef(CalDiagnostic,1) // EM CalDiagnostic information
+    ClassDef(CalDiagnosticData,1) // EM CalDiagnosticData information
 };
 
 #endif
--- digiRootData/EventLinkDef.h
+++ digiRootData/EventLinkDef.h
@@ -14,8 +14,8 @@
 #pragma link C++ class CalDigi+;
 #pragma link C++ class CalXtalReadout+;
 #pragma link C++ class CalXtalId+;
-#pragma link C++ class CalDiagnostic+;
-#pragma link C++ class TkrDiagnostic+;
+#pragma link C++ class CalDiagnosticData+;
+#pragma link C++ class TkrDiagnosticData+;
 #pragma link C++ class EventSummaryData+;
 
 
