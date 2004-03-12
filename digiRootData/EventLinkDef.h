be06f9e3e6966dc3f2e20f263f87baa9e8deb310
berthon <>
2004-03-12 14:34:35 +0000

    add missing classes in EventLinkDef

--- digiRootData/EventLinkDef.h
+++ digiRootData/EventLinkDef.h
@@ -14,6 +14,8 @@
 #pragma link C++ class CalDigi+;
 #pragma link C++ class CalXtalReadout+;
 #pragma link C++ class CalXtalId+;
-
+#pragma link C++ class TkrDiagnosticData+; //class name different from filename!
+#pragma link C++ class CalDiagnosticData+; //class name different from filename!
+#pragma link C++ class EventSummaryData+;
 
 #endif
