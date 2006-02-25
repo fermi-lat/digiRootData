96b40f2ee58b38a97b52f6608a1bf7448b673319
heather <>
2006-02-25 08:23:00 +0000

    clear

--- src/DigiEvent.cxx
+++ src/DigiEvent.cxx
@@ -130,8 +130,11 @@ void DigiEvent::Clear(Option_t *option) {
     m_ebfTimeNanoSec = 0;
     m_ebfUpperPpcTimeBase = 0;
     m_ebfLowerPpcTimeBase = 0;
-    m_levelOneTrigger.Clear();
-    m_summary.Clear();
+    m_levelOneTrigger.Clear("");
+    m_summary.Clear("");
+    m_metaEvent.Clear("");
+
+
     m_calDigiCloneCol->Clear("C");
     /*if (m_calDigiCol) {
         m_calDigiCol->Delete();
