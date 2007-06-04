d13cce1bab58fd5b4b4a9ebfeb2911e4466db316
heather <>
2007-06-04 22:59:12 +0000

    moot keys

---
+++ digiRootData/LsfKeys.h
@@ -0,0 +1,157 @@
+/** 
+ *
+ * @brief Classes encapsulating translated file keys from event data 
+ *
+ * @author Bryson Lee <blee@slac.stanford.edu>
+ *
+ * $Header$
+ */
+
+#ifndef ROOT_LsfKeys_H
+#define ROOT_LsfKeys_H
+
+#include "Riostream.h"
+#include "TObject.h"
+
+#include <vector>
+//#include <iostream>
+//#include <iomanip>
+
+#include "enums/Lsf.h"
+
+
+// forward declare the subclasses
+class LpaKeys;
+class LciKeys;
+
+// abstract base keys class
+class LsfKeys : public TObject {
+
+  public:
+    LsfKeys() : m_LATC_master(0), m_LATC_ignore(0) {};
+
+    LsfKeys( UInt_t master, UInt_t ignore ) 
+      : m_LATC_master( master ), m_LATC_ignore( ignore ) {};
+
+    LsfKeys( const LsfKeys& other )
+      : TObject(other), m_LATC_master( other.LATC_master() ), m_LATC_ignore( other.LATC_ignore() ) {}
+
+    virtual ~LsfKeys() {};
+
+    void Print(Option_t* /*option=""*/) const;
+    void Fake(Int_t ievent, Float_t randNum);
+    Bool_t CompareInRange( const LsfKeys&, const std::string & name = "" ) const ; // for tests
+
+    const LsfKeys& operator=( const LsfKeys& other ) {
+      if ( &other != this ) {
+	m_LATC_master = other.LATC_master();
+	m_LATC_ignore = other.LATC_ignore();
+      }
+      return *this;
+    };
+
+    UInt_t LATC_master() const { return m_LATC_master; };
+    UInt_t LATC_ignore() const { return m_LATC_ignore; };
+
+    void setLATC_master( UInt_t value ) { m_LATC_master = value; };
+    void setLATC_ignore( UInt_t value ) { m_LATC_ignore = value; };
+
+
+    virtual LsfKeys* clone() const = 0;
+    virtual enums::Lsf::KeysType type() const = 0;
+    virtual const LpaKeys* castToLpaKeys() const { return 0; };
+    virtual const LciKeys* castToLciKeys() const { return 0; };
+
+  private:
+    UInt_t m_LATC_master;
+    UInt_t m_LATC_ignore;
+
+    friend class LpaKeys;
+    friend class LciKeys;
+
+    ClassDef(LsfKeys,1) // Base class for LAT keys
+  };
+
+  // translated keys from LPA data
+  class LpaKeys : public LsfKeys {
+
+  public:
+    LpaKeys() : LsfKeys(), m_CDM_keys( std::vector<UInt_t>() ) {};
+
+    LpaKeys( UInt_t master, UInt_t ignore, const std::vector< UInt_t >& dbs )
+      : LsfKeys( master, ignore ), m_CDM_keys( dbs ) {};
+
+    LpaKeys( const LpaKeys& other ) : LsfKeys( other ), m_CDM_keys( other.CDM_keys() ) {};
+
+    virtual ~LpaKeys() {};
+
+    void Print(Option_t* option="" ) const;
+    void Fake(Int_t ievent, Float_t randNum);
+    Bool_t CompareInRange( const LpaKeys&, const std::string & name = "" ) const ; // for tests
+
+    const LpaKeys& operator=( const LpaKeys& other ) {
+      if ( &other != this ) {
+	( dynamic_cast< LsfKeys& >( *this ) ) = other;
+	m_CDM_keys.clear();
+	m_CDM_keys.insert( m_CDM_keys.begin(), other.CDM_keys().begin(), other.CDM_keys().end() );
+      }
+      return *this;
+    }
+
+    const std::vector< UInt_t >& CDM_keys() const { return m_CDM_keys; };
+
+    void setCDM_keys( const std::vector< UInt_t >& value ) {
+      m_CDM_keys.clear();
+      m_CDM_keys.insert( m_CDM_keys.begin(), value.begin(), value.end() );
+    };
+
+    virtual LsfKeys*  clone() const { return new LpaKeys( *this ); };
+    virtual const LpaKeys* castToLpaKeys() const { return this; };
+    virtual enums::Lsf::KeysType type() const { return enums::Lsf::LpaKeys; };
+
+  private:
+    std::vector<UInt_t> m_CDM_keys;
+    ClassDef(LpaKeys,1) // LPA Key
+  };
+
+  // keys from LCI data
+  class LciKeys : public LsfKeys {
+
+  public:
+    LciKeys() : LsfKeys(), m_LCI_script( 0 ) {};
+
+    LciKeys( UInt_t master, UInt_t ignore, UInt_t script )
+      : LsfKeys( master, ignore ), m_LCI_script( script ) {};
+
+    LciKeys( const LciKeys& other ) : LsfKeys( other ), m_LCI_script( other.LCI_script() ) {};
+
+    virtual ~LciKeys() {};
+
+    void Print(Option_t* option="" ) const;
+    void Fake(Int_t ievent, Float_t randNum);
+    Bool_t CompareInRange( const LciKeys&, const std::string & name = "" ) const ; // for tests
+
+    const LciKeys& operator=( const LciKeys& other ) {
+      if ( &other != this ) {
+	( dynamic_cast< LsfKeys& >( *this ) ) = other;
+	m_LCI_script = other.LCI_script();
+      }
+      return *this;
+    };
+
+    UInt_t LCI_script() const { return m_LCI_script; };
+
+    void setLCI_script( UInt_t value ) { m_LCI_script = value; };
+
+
+    virtual LsfKeys*  clone() const { return new LciKeys( *this ); };
+    virtual enums::Lsf::KeysType type() const { return enums::Lsf::LciKeys; };
+    virtual const LciKeys* castToLciKeys() const { return this; };
+
+  private:
+    UInt_t m_LCI_script;
+    ClassDef(LciKeys,1) // LCI Keys
+  };
+
+
+#endif
---
+++ src/LsfKeys.cxx
@@ -0,0 +1,81 @@
+#include "digiRootData/LsfKeys.h"
+#include "commonRootData/RootDataUtil.h"
+#include "Riostream.h"
+
+ClassImp(LsfKeys) 
+
+ClassImp(LpaKeys) 
+ClassImp(LciKeys) 
+
+void LsfKeys::Print(Option_t* /*option*/) const {
+  using namespace std;
+  cout << "LATC_master = 0x " << setw(8) << setfill('0') << hex 
+       << m_LATC_master << endl;
+  cout << "LATC_ignore = 0x" << setw(8) << setfill('0') << hex
+       << m_LATC_ignore << endl;
+}
+
+void LsfKeys::Fake( Int_t ievent, Float_t /* randNum */ ) {
+  m_LATC_master = ievent;
+  m_LATC_ignore = ievent*2;
+}
+
+Bool_t LsfKeys::CompareInRange( const LsfKeys& ref, 
+			        const std::string & name = ""  ) const {
+  /// FIXME
+  bool result = true ;
+ result = rootdatautil::CompareInRange(LATC_master(),ref.LATC_master(),"LATC_master") && result;
+ result = rootdatautil::CompareInRange(LATC_ignore(),ref.LATC_ignore(),"LATC_ignore") && result;
+
+  return result;
+}
+
+void LpaKeys::Print(Option_t* /*option*/) const {
+  /// FIXME
+  using namespace std;
+  cout << " Master = 0x" << std::hex << std::setfill('0')
+       << m_LATC_master << "\n"
+       << " Ignore = 0x" << m_LATC_ignore << std::endl;
+}
+
+void LpaKeys::Fake( Int_t ievent, Float_t randNum ) {
+  LsfKeys::Fake(ievent, randNum);
+  std::vector<unsigned int> vec;
+  unsigned int i;
+  for (i=0; i<10; i++) 
+    vec.push_back(ievent*i);
+  setCDM_keys(vec);
+}
+
+Bool_t LpaKeys::CompareInRange( const LpaKeys& ref, 
+			        const std::string & name = "" ) const {
+  bool result = true ;
+  result = rootdatautil::CompareInRange(CDM_keys(),ref.CDM_keys(),"CDM_keys") && result;
+ result = rootdatautil::CompareInRange(LATC_master(),ref.LATC_master(),"LATC_master") && result;
+ result = rootdatautil::CompareInRange(LATC_ignore(),ref.LATC_ignore(),"LATC_ignore") && result;
+  return result;
+}
+
+
+void LciKeys::Print(Option_t* /*option*/) const {
+  /// FIXME
+  using namespace std;
+  cout << "Master = 0x" << std::hex << std::setfill('0')
+       << m_LATC_master << endl
+       << "Ignore: " << m_LATC_ignore << endl;
+}
+
+void LciKeys::Fake( Int_t ievent, Float_t randNum ) {
+  LsfKeys::Fake(ievent, randNum);
+  setLCI_script(ievent);
+}
+
+Bool_t LciKeys::CompareInRange( const LciKeys& ref, 
+        const std::string & name = "") const {
+  bool result = true ;
+  result = rootdatautil::CompareInRange(LCI_script(),ref.LCI_script(),"LCI_script") && result;
+ result = rootdatautil::CompareInRange(LATC_master(),ref.LATC_master(),"LATC_master") && result;
+ result = rootdatautil::CompareInRange(LATC_ignore(),ref.LATC_ignore(),"LATC_ignore") && result;
+  return result;
+}
+
