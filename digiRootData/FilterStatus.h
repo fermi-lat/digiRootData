d8b3d2b3fbb06bd6b6adbae424165023761d27a0
usher <>
2007-08-28 23:32:30 +0000

    Update FilterStatus.h to reflect changes in the Onboard Filter tracking structures made with release v1-0-0. The tracking objects are mostly renamed, there should be no conflict with previous versions except for TfcProjectionCol

--- digiRootData/FilterStatus.h
+++ digiRootData/FilterStatus.h
@@ -51,84 +51,261 @@ private:
 };
 
 
-/** @class TfcProjection
-@brief   
+/** @class TfcHit
+@brief This is the root version of the FSW structure TFC_hit
 */
-class TfcProjection : public TObject {
+class TfcHit : public TObject {
 public:
-    TfcProjection() { };
+    TfcHit() : m_strip(0), m_tower(0), m_idx(0) 
+               { }
 
-    TfcProjection(Int_t intercept, Int_t slope, Int_t acdTopMask, 
-                   Int_t acdXMask, Int_t acdYMask, UInt_t layers, 
-                   Short_t* hits, UChar_t skirtMask, UChar_t min, UChar_t max, 
-                   UChar_t nhits);
+    TfcHit(Short_t strip, UChar_t tower, UChar_t idx) :
+               m_strip(strip), m_tower(tower), m_idx(idx) 
+               { }
 
-    TfcProjection(const TfcProjection& copy);
+    TfcHit(const TfcHit& copy)
+    {
+        m_strip = copy.m_strip;
+        m_tower = copy.m_tower;
+        m_idx   = copy.m_idx;
+    }
 
     /// define assignment operator
-    TfcProjection& operator=(const TfcProjection& copy); 
+    TfcHit& operator=(const TfcHit& copy)
+    {
+        initialize(copy.m_strip, copy.m_tower, copy.m_idx);
+        return *this;
+    }
 
-    virtual ~TfcProjection() {};
-    void initialize(Int_t intercept, Int_t slope, Int_t acdTopMask, 
-                    Int_t acdXMask, Int_t acdYMask, UInt_t layers,
-                    const Short_t* hits, UChar_t skirtMask, UChar_t min, 
-                    UChar_t max, UChar_t nhits);
+    virtual ~TfcHit() {};
+    void initialize(Short_t strip, UChar_t tower, UChar_t idx)
+    {
+        m_strip = strip;
+        m_tower = tower;
+        m_idx   = idx;
+    }
+
+    void Clear(Option_t *option ="")
+    {
+        m_strip = 0;
+        m_tower = 0;
+        m_idx   = 0;
+    }
+
+    void Print(Option_t *option="") const;
+
+    inline Short_t getStrip() const {return m_strip;}
+    inline UChar_t getTower() const {return m_tower;}
+    inline UChar_t getIdx()   const {return m_idx;}
+
+private:
+    /// data members
+    Short_t m_strip;    // hit strip id
+    UChar_t m_tower;    // hit tower
+    UChar_t m_idx;      // index
+
+    ClassDef(TfcHit,1) // TfcHit
+};
+
+/** @class TfcPrjList
+@brief This is the root version of the FSW structure TFC_prjList but 
+       serves only to hold the pointers, we do not "rootify" them
+*/
+class TfcPrjList : public TObject {
+public:
+    TfcPrjList() : m_flnk(0), m_blnk(0)
+               { }
+
+    TfcPrjList(void* flnk, void* blnk) :
+               m_flnk(flnk), m_blnk(blnk)
+               { }
+
+    TfcPrjList(const TfcPrjList& copy)
+    {
+        m_flnk = copy.m_flnk;
+        m_blnk = copy.m_blnk;
+    }
+
+    /// define assignment operator
+    TfcPrjList& operator=(const TfcPrjList& copy)
+    {
+        m_flnk = copy.m_flnk;
+        m_blnk = copy.m_blnk;
+        return *this;
+    }
+
+    virtual ~TfcPrjList() {};
+    void initialize(void* flnk, void* blnk)
+    {
+        m_flnk = flnk;
+        m_blnk = blnk;
+    }
+
+    void Clear(Option_t *option ="")
+    {
+        m_flnk = 0;
+        m_blnk = 0;
+    }
+
+    void Print(Option_t *option="") const;
+
+    inline void* getFlnk() const {return m_flnk;}
+    inline void* getBlnk() const {return m_blnk;}
+
+private:
+    /// data members
+    void* m_flnk;    // forward link
+    void* m_blnk;    // Backward link
+
+    ClassDef(TfcPrjList,1) // TfcPrjLink
+};
+
+
+/** @class TfcPrjPrms
+@brief This is the root version of the FSW structure TFC_prjPrms
+*/
+class TfcPrjPrms : public TObject {
+public:
+    TfcPrjPrms() : m_xy0(0), m_dxy(0), m_dz(0), m_tower(0), m_layer(0) 
+               { }
+
+    TfcPrjPrms(Short_t xy0, Short_t dxy, UShort_t dz, UChar_t tower, UChar_t layer) :
+               m_xy0(xy0), m_dxy(dxy), m_dz(dz), m_tower(tower), m_layer(layer) 
+               { }
+
+    TfcPrjPrms(const TfcPrjPrms& copy)
+    {
+        m_xy0   = copy.m_xy0;
+        m_dxy   = copy.m_dxy;
+        m_dz    = copy.m_dz;
+        m_tower = copy.m_tower;
+        m_layer = copy.m_layer;
+    }
+
+    /// define assignment operator
+    TfcPrjPrms& operator=(const TfcPrjPrms& copy)
+    {
+        initialize(copy.m_xy0, copy.m_dxy, copy.m_dz, copy.m_tower, copy.m_layer);
+        return *this;
+    }
+
+    virtual ~TfcPrjPrms() {};
+    void initialize(Short_t xy0, Short_t dxy, UShort_t dz, UChar_t tower, UChar_t layer)
+    {
+        m_xy0   = xy0;
+        m_dxy   = dxy;
+        m_dz    = dz;
+        m_tower = tower;
+        m_layer = layer;
+    }
+
+    void Clear(Option_t *option ="")
+    {
+        m_xy0   = 0;
+        m_dxy   = 0;
+        m_dz    = 0;
+        m_tower = 0;
+        m_layer = 0;
+    }
+
+    void Print(Option_t *option="") const;
+
+    inline Short_t  getxy0()   const {return m_xy0;}
+    inline Short_t  getdxy()   const {return m_dxy;}
+    inline UShort_t getdz()    const {return m_dz;}
+    inline UChar_t  getTower() const {return m_tower;}
+    inline UChar_t  getLayer() const {return m_layer;}
+
+private:
+    /// data members
+    Short_t  m_xy0;     // Position at the top/bottom layer
+    Short_t  m_dxy;     // Change in strip number
+    UShort_t m_dz;      // Change in z layer position 
+
+    UChar_t  m_tower;   // Tower
+    UChar_t  m_layer;   // layer
+
+    ClassDef(TfcPrjPrms,1) // TfcHit
+};
+
+
+/** @class TfcPrj
+@brief   Implements root version of FSW structure TFC_prj
+*/
+class TfcPrj : public TObject {
+public:
+    TfcPrj() { };
+
+    TfcPrj(TfcPrjPrms& top, TfcPrjPrms& bot, Int_t acdTopMask, Int_t acdXMask,
+           Int_t acdYMask, UChar_t skirtMask, UChar_t min, UChar_t max, UChar_t nHits,
+           UInt_t layers, TfcHit* hits);
+
+    TfcPrj(const TfcPrj& copy);
+
+    /// define assignment operator
+    TfcPrj& operator=(const TfcPrj& copy); 
+
+    virtual ~TfcPrj() {};
+    void initialize(const TfcPrjPrms& top, const TfcPrjPrms& bot, Int_t acdTopMask, 
+                    Int_t acdXMask, Int_t acdYMask, UChar_t skirtMask,
+                    UChar_t min, UChar_t max, UChar_t nHits, UInt_t layers,
+                    const TfcHit* hits);
 
     void Clear(Option_t *option ="");
 
     void Print(Option_t *option="") const;
 
 
-    inline Int_t getIntercept() const { return m_intercept; }
-    inline Int_t getSlope() const { return m_slope; }
-    inline Int_t getAcdTopMask() const { return m_acdTopMask; }
-    inline Int_t getAcdXMask() const { return m_acdXMask; }
-    inline Int_t getAcdYMask() const { return m_acdYMask; }
-    inline UInt_t getLayers() const { return m_layers; }
-    inline const Short_t* getHits() const { return m_hits; }
-    inline UChar_t getSkirtMask() const { return m_skirtMask; }
-    inline UChar_t getMin() const { return m_min; }
-    inline UChar_t getMax() const { return m_max; }
-    inline UChar_t getNhits() const { return m_nhits; }
+    inline const TfcPrjPrms& getTop()        const { return m_top;}
+    inline const TfcPrjPrms& getBot()        const { return m_bot;}
+    inline Int_t             getAcdTopMask() const { return m_acdTopMask; }
+    inline Int_t             getAcdXMask()   const { return m_acdXMask; }
+    inline Int_t             getAcdYMask()   const { return m_acdYMask; }
+    inline UChar_t           getSkirtMask()  const { return m_skirtMask; }
+    inline UChar_t           getMin()        const { return m_min; }
+    inline UChar_t           getMax()        const { return m_max; }
+    inline UChar_t           getNhits()      const { return m_nhits; }
+    inline UInt_t            getLayers()     const { return m_layers; }
+    inline const TfcHit*     getHits()       const { return m_hits; }
 
 private:
-    /// intercept at beginning layer
-    Int_t  m_intercept;
-    Int_t  m_slope;
+    /// projection parameters
+    TfcPrjPrms m_top;
+    TfcPrjPrms m_bot;
     /// ACD top tile candidates
-    Int_t  m_acdTopMask; 
+    Int_t      m_acdTopMask; 
     /// ACD x facing candidates
-    Int_t  m_acdXMask; 
+    Int_t      m_acdXMask; 
     /// ACD y facing candidates
-    Int_t  m_acdYMask; 
-    /// Bit mask representing the struck layers
-    UInt_t m_layers;
-    /// Hits assigned to projection
-    Short_t m_hits[18]; 
+    Int_t      m_acdYMask; 
     /// Mask of which skirt region projection strikes
-    UChar_t m_skirtMask; 
+    UChar_t    m_skirtMask; 
     /// Beginning layer number of the projection
-    UChar_t m_min; 
+    UChar_t    m_min; 
     /// Ending layer number of the projection
-    UChar_t m_max; 
+    UChar_t    m_max; 
     /// Number of hits assigned
-    UChar_t m_nhits;
+    UChar_t    m_nhits;
+    /// Bit mask representing the struck layers
+    UInt_t     m_layers;
+    /// Hits assigned to projection
+    TfcHit     m_hits[18]; 
 
-    ClassDef(TfcProjection,1) // TfcProjection
+    ClassDef(TfcPrj,1) // TfcProjection
 };
 
-/** @class TfcProjectionDir
+/** @class TfcPrjDir
 @brief   
 */
-class TfcProjectionDir: public TObject {
+class TfcPrjDir: public TObject {
 public:
-    TfcProjectionDir() { };
-    TfcProjectionDir(UShort_t idx, UChar_t x, UChar_t y);
-    TfcProjectionDir(const TfcProjectionDir& copy);
+    TfcPrjDir() { };
+    TfcPrjDir(UShort_t idx, UChar_t x, UChar_t y);
+    TfcPrjDir(const TfcPrjDir& copy);
     /// define assignment operator
-    TfcProjectionDir& operator=(const TfcProjectionDir& copy); 
+    TfcPrjDir& operator=(const TfcPrjDir& copy); 
 
-    virtual ~TfcProjectionDir(){ };
+    virtual ~TfcPrjDir(){ };
 
     void initialize(UShort_t idx, UChar_t x, UChar_t y);
 
@@ -147,7 +324,7 @@ private:
     UChar_t   m_xCnt; 
     /// Count of Y projections
     UChar_t   m_yCnt; 
-    ClassDef(TfcProjectionDir,1) // TfcProjectionDir
+    ClassDef(TfcPrjDir,1) // TfcPrjDir
 };
 
 
@@ -156,10 +333,13 @@ private:
 */
 class TfcProjectionCol : public TObject {
 public:
-    TfcProjectionCol():TObject() { m_maxCnt = 0; m_prjs = 0; Clear(""); };
+    TfcProjectionCol():TObject(), m_maxCnt(0), m_curCnt(0), m_twrMsk(0)
+    {
+        Clear(""); 
+    };
     
-    TfcProjectionCol(Int_t maxCnt, UShort_t curCnt, UShort_t twrMas, 
-        const TfcProjectionDir *dir, const TfcProjection* projectionCol);
+    TfcProjectionCol(UShort_t maxCnt, UShort_t curCnt, UShort_t twrMas, 
+        const TfcPrjDir *dir, const TfcPrjList* topDir, const TfcPrjList* botDir, const TfcPrj* projectionCol);
 
     TfcProjectionCol(const TfcProjectionCol& other);
 
@@ -169,7 +349,7 @@ public:
     virtual ~TfcProjectionCol();
 
     void initialize(Int_t maxCnt, UShort_t curCnt, UShort_t twrMas, 
-        const TfcProjectionDir *dir, const TfcProjection* projectionCol);
+        const TfcPrjDir *dir, const TfcPrjList* topDir, const TfcPrjList* botDir, const TfcPrj* projectionCol);
 
     void Clear(Option_t *option ="");
 
@@ -177,24 +357,28 @@ public:
 
     void Fake( Int_t ievent, Float_t randNum ); 
 
-    inline Int_t getMaxCount() const { return m_maxCnt; };
-    inline UShort_t getCurrentCount() const { return m_curCnt; };
-    inline UShort_t getTwrMsk() const { return m_twrMsk; };
-    inline const TfcProjectionDir* getProjectionDirCol() const { return m_dir; }
-    inline const TfcProjection* getProjectionCol() const { return m_prjs; }
+    inline UShort_t           getMaxCount()         const { return m_maxCnt; };
+    inline UShort_t           getCurrentCount()     const { return m_curCnt; };
+    inline UShort_t           getTwrMsk()           const { return m_twrMsk; };
+    inline const TfcPrjDir*   getProjectionDirCol() const { return m_dir; }
+    inline const TfcPrjList*  getTopList()          const { return m_top;}
+    inline const TfcPrjList*  getBotList()          const { return m_bot;}
+    inline const TfcPrj*      getProjectionCol()    const { return m_prjs; }
 
 private:
     ///Maximum # of projections available
-    Int_t    m_maxCnt; 
+    UShort_t    m_maxCnt; 
     //Current # of projections in use   
-    Int_t    m_curCnt;
+    UShort_t    m_curCnt;
     ///Mask of the tower with projections  
     UShort_t    m_twrMsk; 
     /// Directory of the projections by twr 
-    TfcProjectionDir    m_dir[16];
-    TfcProjection* m_prjs; //[m_curCnt]
+    TfcPrjDir   m_dir[16];
+    TfcPrjList  m_top[32];
+    TfcPrjList  m_bot[32];
+    TfcPrj*     m_prjs; //[m_curCnt]
 
-    ClassDef(TfcProjectionCol,3) // TfcProjectionCol
+    ClassDef(TfcProjectionCol,4) // TfcProjectionCol
 };
 
 class ObfTrack : public TObject {
