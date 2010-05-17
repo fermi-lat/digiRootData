11e7a73a0fc55ca265389141ff6ab7e611f5dbdb
usher <>
2010-05-17 19:17:09 +0000

    Fix memory leak found on linux with the DigiObjectManager system

--- src/TkrDigi.cxx
+++ src/TkrDigi.cxx
@@ -100,6 +100,12 @@ void* TkrDigi::operator new(size_t /*size*/)
     // Since we recycle, make sure these member functions are cleared
     temp->m_hitCol.clear();
 
+    // Note that to make sure memory is deallocated on linux we need 
+    // to use this trick of "swapping" vectors... most annoying
+    std::vector<UInt_t> swapVec;
+
+    temp->m_hitCol.swap(swapVec);
+
     return temp;
 }
 
