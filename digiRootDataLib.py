72c33adb6780d33c9c545c95ae4c4d9b72c81ac1
jrb <>
2011-01-06 22:45:51 +0000

    windows only: add localIncludes arg. to Rootcint

--- digiRootDataLib.py
+++ digiRootDataLib.py
@@ -3,7 +3,6 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['digiRootData'])
     env.Tool('commonRootDataLib')
-    env.Tool('facilitiesLib')
     env.Tool('addLibrary', library = env['ldfLibs'])
     env.Tool('lsfDataLib')
     env.Tool('addLibrary', library = env['obfLibs'])
