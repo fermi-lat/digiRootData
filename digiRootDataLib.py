f570efc8d5df4bed47fd9794c72ca8b10cfca242
jrb <>
2011-10-20 01:11:24 +0000

    Avoid dragging in libraries unnecessarily

--- digiRootDataLib.py
+++ digiRootDataLib.py
@@ -3,10 +3,6 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['digiRootData'])
     env.Tool('commonRootDataLib')
-    env.Tool('facilitiesLib')
     env.Tool('addLibrary', library = env['ldfLibs'])
-    env.Tool('lsfDataLib')
-    env.Tool('addLibrary', library = env['obfLibs'])
-    env.Tool('addLibrary', library = env['rootLibs'])
 def exists(env):
     return 1;
