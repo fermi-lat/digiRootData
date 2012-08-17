60044ae580664bbabb652fd569494741d553f4f7
jrb <>
2012-08-17 19:45:25 +0000

    patches for Windows no-install-headers

--- digiRootDataLib.py
+++ digiRootDataLib.py
@@ -2,7 +2,12 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['digiRootData'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'digiRootData') 
     env.Tool('commonRootDataLib')
     env.Tool('addLibrary', library = env['ldfLibs'])
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'enums') 
+
 def exists(env):
     return 1;
